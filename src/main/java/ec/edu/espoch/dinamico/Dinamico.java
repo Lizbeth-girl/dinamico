f383a473f77138380bb2505727d3ba118d41fc0e
Lizbeth-girl <saezlizbethe6@gamail.com>
2025-05-12 11:32:10 -0500

    pratica

---
+++ src/main/java/ec/edu/espoch/dinamico/Dinamico.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.dinamico;
+
+import ec.edu.espoch.dinamico.clases.Estudiante;
+
+/**
+ *
+ * @author PERSONAL
+ */
+public class Dinamico {
+
+    public static void main(String[] args) {
+     
+        Estudiante objEstudiante = new Estudiante()
+    }
+}
